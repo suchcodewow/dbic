f8519f71398c9a158a50d6aea876d5a9e500f1c0
Shawn Pearson <1157wcl@gmail.com>
2022-07-18 14:11:43 -0400

    catalog update'

--- reset.ps1
+++ reset.ps1
@@ -1,5 +1,12 @@
+# Tear down Docker images
 docker ps -q | ForEach-Object { docker stop $_ }
 docker images -a -q | ForEach-Object { docker rmi $_ }
 docker system prune -f
-docker build --pull --rm -f "catalog_mysqldb/Dockerfile" -t dbic:latest "catalog_mysqldb"
-docker run --rm -d  -p 3306:3306/tcp -p 33060:33060/tcp dbic:latest
+
+# Build Catalog/mysql
+docker build --rm -f "catalog_mysqldb/Dockerfile" -t dbiccatdb:latest "catalog_mysqldb"
+docker run --rm -d  -p 3306:3306/tcp -p 33060:33060/tcp dbiccatdb:latest
+
+# Build Catalog/Api
+docker build --rm -f "catalogApi/Dockerfile" -t dbiccatapi:latest "catalogApi"
+docker run --rm -d -p 8080:8080/tcp dbiccatapi:latest

321eb507bcd9f4ccaf081b54ffd6d893735bc26d
Shawn Pearson <1157wcl@gmail.com>
2022-07-20 22:28:38 -0400

    docker compose

--- reset.ps1
+++ reset.ps1
@@ -4,6 +4,9 @@ docker images -a -q | ForEach-Object { docker rmi $_ }
 docker system prune -f
 
 # Build Catalog/mysql
+Push-Location .\catalogApi
+.\mvnw clean install -DskipTests=true
+Pop-Location
 docker build --rm -f "catalog_mysqldb/Dockerfile" -t dbiccatdb:latest "catalog_mysqldb"
 docker run --rm -d  -p 3306:3306/tcp -p 33060:33060/tcp dbiccatdb:latest
 

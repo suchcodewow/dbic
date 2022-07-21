f5f2e3eb6b54a25d27aaa21c5143a543ea4eb18a
Shawn Pearson <1157wcl@gmail.com>
2022-07-21 15:33:03 -0400

    docker compose

--- breset.sh
+++ breset.sh
@@ -5,18 +5,21 @@
 #kill $(ps -ef | grep npm | grep -v grep | awk '{ print $2 }')
 
 # Tear down Docker images
-docker kill $(docker ps -q)
-docker rmi $(docker ps -q -a)
-docker system prune -f
+docker compose down
+docker compose rm -f
+#docker kill $(docker ps -q)
+#docker rmi $(docker ps -q -a)
+#docker system prune -f
 
 # Build Catalog/mysql
-docker build --rm -f "catalog_mysqldb/Dockerfile" -t dbiccatdb:latest "catalog_mysqldb"
-docker run --rm -d -p 3306:3306/tcp -p 33060:33060/tcp dbiccatdb:latest
+#docker build --rm -f "catalog_mysqldb/Dockerfile" -t dbiccatdb:latest "catalog_mysqldb"
+#docker run --rm -d -p 3306:3306/tcp -p 33060:33060/tcp dbiccatdb:latest
 
 # Build Catalog/Api
-docker build --rm -f "catalogApi/Dockerfile" -t dbiccatapi:latest "catalogApi"
-docker run --rm -d -p 8080:8080/tcp dbiccatapi:latest
+#docker build --rm -f "catalogApi/Dockerfile" -t dbiccatapi:latest "catalogApi"
+#docker run --rm -d -p 8080:8080/tcp dbiccatapi:latest
 
 #Start Apps
-#mvn -f ./catalogApi/ spring-boot:run >catalogApi.log &
+mvn -f ./catalogApi/ clean install -DskipTests=true >catalogApi.log
+docker compose up --build -d
 #npm run dev --prefix ./ui/ >ui.log &

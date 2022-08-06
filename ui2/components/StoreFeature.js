87963ecb47639437ae0d6ef04af30977cb05971f
Shawn Pearson <1157wcl@gmail.com>
2022-08-06 15:19:29 -0400

    endless cart

--- ui2/components/StoreFeature.js
+++ ui2/components/StoreFeature.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { DefaultArea } from "styles/Common";
+import ReactStars from "react-stars";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 
 const { publicRuntimeConfig } = require("next.config");
 
@@ -8,6 +10,19 @@ export default function StoreFeature() {
   const BASE_URL = publicRuntimeConfig.apiCatalog;
   const [catalog, setCatalog] = useState(null);
   const [loading, setLoading] = useState(null);
+  const [cartItems, setCartItems] = useState(null);
+  const onAdd = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
   useEffect(() => {
     var localCart = JSON.parse(localStorage.getItem("cart"));
     if (localCart) {
@@ -40,15 +55,17 @@ export default function StoreFeature() {
       <StoreContainer>
         <Allitems>
           {catalog?.map((item) => (
-            <ItemContainer key={item.id}>
+            <ItemContainer key={item.id} onClick={() => onAdd(item)}>
               <ItemImage>
                 <img src={`/images/store/${item.img}`} alt="" />
               </ItemImage>
               <ItemContent>
-                <div>
+                <PriceLine>
                   <h3>${item.price}</h3>
-                  <h4>.00</h4>
-                </div>
+                  <sup>.00</sup>
+                  &nbsp;&nbsp;
+                  <ReactStars value={item.rating} count="5" half />
+                </PriceLine>
                 <p>{item.shortDesc}</p>
               </ItemContent>
             </ItemContainer>
@@ -59,24 +76,29 @@ export default function StoreFeature() {
   );
 }
 
-const ItemContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  padding: 1rem 1rem;
-  height: 200px;
-  p {
-    font-size: 14px;
-    font-weight: 700;
-
-    color: #fff;
-  }
+const PriceLine = styled.div`
+  width: 100%;
+  color: #fff;
   h3 {
     font-size: 25px;
     font-weight: 800;
     line-height: 30px;
-    color: #fff;
+    display: inline;
   }
+`;
+const ItemContent = styled.div`
+  display: flex;
+  padding: 10px;
+  flex-direction: column;
+
+  height: 200px;
+  p {
+    font-size: 14px;
+    font-weight: 700;
+    color: #fff;
+    margin-top: 1rem;
+  }
+
   p:last-child {
     cursor: pointer;
   }
@@ -92,16 +114,17 @@ const ItemImage = styled.div`
   img {
     position: absolute;
     height: 100%;
-    margin: auto;
+    padding: 5px 5px;
   }
 `;
 
 const ItemContainer = styled.div`
   width: 300px;
-  height: 100%;
+  height: 400px;
+
   display: flex;
   flex-direction: column;
-  background: #031b34;
+  background: #05284d;
 `;
 
 const Allitems = styled.div`

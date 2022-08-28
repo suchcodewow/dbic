6398ab07496a39fdd7a4494e8f81f1019b1f5cd4
SuchCodeWow <1157wcl@gmail.com>
2022-08-28 13:34:23 -0400

    OrdersAPI Docker Config

--- OrdersApi/OrdersAPI/Program.cs
+++ OrdersApi/OrdersAPI/Program.cs
@@ -9,15 +9,24 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-    //options.UseSqlServer("Server=OrdersDb;Database=master;User=sa;Password=Password123");
+    //Connect to local docker built with:
+    //docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=yourStrong(!)Password" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
+    //options.UseSqlServer("Server=localhost;Database=master;User=sa;Password=yourStrong(!)Password");
+    //Connect to docker built with docker-compose
     // options.UseSqlServer("Server=OrdersDb;Database=Ordersdb;User=sa;Password=Password123");
 }
 );
+System.Diagnostics.Debug.WriteLine("hello");
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DataContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- reset.ps1
+++ reset.ps1
@@ -15,3 +15,5 @@ docker system prune -f
 #docker run --rm -d -p 8080:8080/tcp dbiccatapi:latest
 
 #docker build -f "C:\Users\admin\source\dbic\OrdersApi\OrdersAPI\Dockerfile" --force-rm -t ordersapi:dev --target base  --label "com.microsoft.created-by=visual-studio" --label "com.microsoft.visual-studio.project-name=OrdersAPI" "C:\Users\admin\source\dbic\OrdersApi" 
+
+#docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=yourStrong(!)Password" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest

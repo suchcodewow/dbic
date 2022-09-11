88c46ffffb841141b7241ddad1fc2f1b6103b9d1
SuchCodeWow <1157wcl@gmail.com>
2022-09-11 16:06:39 -0400

    insurance landing

--- ui/pages/insurance.jsx
+++ ui/pages/insurance.jsx
@@ -1,10 +1,80 @@
 import Nav from "components/Nav";
+import {
+  BoltIcon,
+  ChatBubbleBottomCenterTextIcon,
+  GlobeAltIcon,
+  ScaleIcon,
+  HomeIcon,
+  KeyIcon,
+} from "@heroicons/react/24/outline";
+
+const features = [
+  {
+    name: "Competitive exchange rates",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: HomeIcon,
+  },
+  {
+    name: "No hidden fees",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: KeyIcon,
+  },
+  {
+    name: "Transfers are instant",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: BoltIcon,
+  },
+  {
+    name: "Mobile notifications",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: ChatBubbleBottomCenterTextIcon,
+  },
+];
 
 export default function Insurance() {
   return (
     <div>
       <Nav />
-      <div>insurance</div>
+      <div className="bg-white py-12">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="lg:text-center">
+            <h2 className="text-lg font-semibold text-indigo-600">
+              Transactions
+            </h2>
+            <p className="mt-2 text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
+              A better way to send money
+            </p>
+            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+              Lorem ipsum dolor sit amet consect adipisicing elit. Possimus
+              magnam voluptatum cupiditate veritatis in accusamus quisquam.
+            </p>
+          </div>
+
+          <div className="mt-10">
+            <dl className="space-y-10 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10 md:space-y-0">
+              {features.map((feature) => (
+                <div key={feature.name} className="relative">
+                  <dt>
+                    <div className="absolute flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
+                      <feature.icon className="h-6 w-6" aria-hidden="true" />
+                    </div>
+                    <p className="ml-16 text-lg font-medium leading-6 text-gray-900">
+                      {feature.name}
+                    </p>
+                  </dt>
+                  <dd className="mt-2 ml-16 text-base text-gray-500">
+                    {feature.description}
+                  </dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

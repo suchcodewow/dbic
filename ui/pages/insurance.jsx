e82c83d166764e01acc45d14a59e9ec2565608c7
SuchCodeWow <1157wcl@gmail.com>
2022-09-12 08:05:33 -0400

    insurance hero

--- ui/pages/insurance.jsx
+++ ui/pages/insurance.jsx
@@ -10,28 +10,22 @@ import {
 
 const features = [
   {
-    name: "Competitive exchange rates",
+    name: "Comprehensive Home Coverage",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      "Homeowners insurance protects you from fires, lightning, wind, thefts and vandalism.",
     icon: HomeIcon,
   },
   {
-    name: "No hidden fees",
+    name: '"No-surprise guarantee"',
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      "You'll pay nothing beyond your agreed on deductible if your vehicle is involved in an accident- guaranteed.",
     icon: KeyIcon,
   },
   {
-    name: "Transfers are instant",
+    name: "Fast Claim Response",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      "You'll be whole again fast with the fastest claim service on the planet.  ",
     icon: BoltIcon,
-  },
-  {
-    name: "Mobile notifications",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-    icon: ChatBubbleBottomCenterTextIcon,
   },
 ];
 
@@ -39,40 +33,35 @@ export default function Insurance() {
   return (
     <div>
       <Nav />
-      <div className="bg-white py-12">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="lg:text-center">
-            <h2 className="text-lg font-semibold text-indigo-600">
-              Transactions
-            </h2>
-            <p className="mt-2 text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
-              A better way to send money
-            </p>
-            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-              Lorem ipsum dolor sit amet consect adipisicing elit. Possimus
-              magnam voluptatum cupiditate veritatis in accusamus quisquam.
-            </p>
-          </div>
-
-          <div className="mt-10">
-            <dl className="space-y-10 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10 md:space-y-0">
-              {features.map((feature) => (
-                <div key={feature.name} className="relative">
-                  <dt>
-                    <div className="absolute flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
-                      <feature.icon className="h-6 w-6" aria-hidden="true" />
-                    </div>
-                    <p className="ml-16 text-lg font-medium leading-6 text-gray-900">
-                      {feature.name}
-                    </p>
-                  </dt>
-                  <dd className="mt-2 ml-16 text-base text-gray-500">
-                    {feature.description}
-                  </dd>
-                </div>
-              ))}
-            </dl>
-          </div>
+      <div className="bg-white  mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="lg:text-center">
+          <p className="pt-8 text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
+            Insurance that fits like a glove
+          </p>
+          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            Protecting what matters to you shouldn't be a hassle. Find the
+            coverage you need and get the peace of mind you deserve.
+          </p>
+        </div>
+        <div className="mt-10">
+          <dl className="space-y-10 md:grid md:grid-cols-3 md:gap-x-14  md:gap-y-10 md:space-y-0">
+            {features.map((feature) => (
+              <div key={feature.name} className="relative">
+                <dt>
+                  <div className="absolute flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
+                    <feature.icon className="h-6 w-6" aria-hidden="true" />
+                  </div>
+                  <p className="ml-16 text-lg font-medium leading-6 text-gray-900">
+                    {feature.name}
+                  </p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  {feature.description}
+                </dd>
+              </div>
+            ))}
+          </dl>
+          <div className="py-10">CTA</div>
         </div>
       </div>
     </div>
--- ui/pages/myaccount.jsx
+++ ui/pages/myaccount.jsx
@@ -35,119 +35,115 @@ export default function MyAccount() {
     <div>
       <Nav />
       {/* SideBar */}
-      <div className="w-full z-10 top-0">
-        <div className="mx-auto h-fit max-w-7xl px-2 sm:px-6 lg:px-8 flex">
-          <aside className="pl-4 py-4 w-1/3 sticky" aria-label="Sidebar">
-            <div className="overflow-y-auto py-4 px-3 bg-azure-700 text-white rounded-md">
-              <ul className="space-y-2">
-                <li className="mb-6">
-                  <span className="ml-1 font-bold">Overview</span>
-                </li>
-                <li>
-                  <div className="flex">
-                    <CreditCardIcon className="w-5" />
-                    <span className="font-bold mr-1 ml-3 whitespace-nowrap text-transparent bg-clip-text bg-gradient-to-r from-[#ae67fa] to-[#f49867]">
-                      DYNACARD
-                    </span>
-                    <span>status: </span>
-                    <span className="inline-flex justify-center items-center px-2 ml-3 text-xs font-medium text-gray-800 shadow-md bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">
-                      APPROVED
-                    </span>
-                  </div>
-                </li>
-                <li>
-                  <div className="flex ">
-                    <InboxIcon className="w-5" />
-                    <span className="flex-1 ml-3 whitespace-nowrap">
-                      Unread Messages
-                    </span>
-                    <span className="inline-flex justify-center items-center p-3 ml-3 w-3 h-3 text-sm font-medium  bg-azure-600 shadow-md rounded-full ">
-                      0
-                    </span>
-                  </div>
-                </li>
-                <li>
-                  <a
-                    href="/store"
-                    className="flex items-center p-2 text-base font-normal rounded-lg  hover:bg-azure-500"
-                  >
-                    <ShoppingBagIcon className="w-5" />
-                    <span className="flex-1 ml-3 whitespace-nowrap">Store</span>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/banking"
-                    className="flex items-center p-2 text-base font-normal rounded-lg  hover:bg-azure-500"
-                  >
-                    <BanknotesIcon className="w-5" />
-                    <span className="flex-1 ml-3 whitespace-nowrap">
-                      Banking
-                    </span>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/insurance"
-                    className="flex items-center p-2 text-base font-normal rounded-lg  hover:bg-azure-500"
-                  >
-                    <HomeIcon className="w-5" />
-                    <span className="flex-1 ml-3 whitespace-nowrap">
-                      Insurance
-                    </span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </aside>
-          <div className="mx-auto bg-white border  rounded-md m-4 w-full ml-5 p-5">
-            <div className="mb-4 font-bold text-xl">My Orders</div>
-            {/* table */}
-            <div className="overflow-x-auto relative shadow-sm sm:rounded-lg">
-              <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-200">
-                  <tr>
-                    <th scope="col" className="py-3 px-6">
-                      Order
-                    </th>
-                    <th scope="col" className="py-3 px-6">
-                      Status
-                    </th>
-                    <th scope="col" className="py-3 px-6">
-                      Total Items
-                    </th>
-                    <th scope="col" className="py-3 px-6">
-                      Total Cost
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {orders?.map((order) => (
-                    <tr key={order.id} className="border-b bg-gray-100 ">
-                      <th
-                        scope="row"
-                        className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap "
-                      >
-                        #{order.id}
-                      </th>
-                      <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap">
-                        {order.status}
-                      </td>
-                      <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap">
-                        {order.totalItems}
-                      </td>
-                      <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap">
-                        ${order.cartTotal}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-
-            <div className="my-4 font-bold text-xl">Bank Accounts</div>
-            <div className="my-4 font-bold text-xl">Insurance Policies</div>
+      <div className="mx-auto h-fit max-w-7xl px-2 sm:px-6 lg:px-8 flex">
+        <aside className="pl-4 py-4 w-1/3 sticky" aria-label="Sidebar">
+          <div className="overflow-y-auto py-4 px-3 bg-azure-700 text-white rounded-md">
+            <ul className="space-y-2">
+              <li className="mb-6">
+                <span className="ml-1 font-bold">Overview</span>
+              </li>
+              <li>
+                <div className="flex">
+                  <CreditCardIcon className="w-5" />
+                  <span className="font-bold mr-1 ml-3 whitespace-nowrap text-transparent bg-clip-text bg-gradient-to-r from-[#ae67fa] to-[#f49867]">
+                    DYNACARD
+                  </span>
+                  <span>status: </span>
+                  <span className="inline-flex justify-center items-center px-2 ml-3 text-xs font-medium text-gray-800 shadow-md bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">
+                    APPROVED
+                  </span>
+                </div>
+              </li>
+              <li>
+                <div className="flex ">
+                  <InboxIcon className="w-5" />
+                  <span className="flex-1 ml-3 whitespace-nowrap">
+                    Unread Messages
+                  </span>
+                  <span className="inline-flex justify-center items-center p-3 ml-3 w-3 h-3 text-sm font-medium  bg-azure-600 shadow-md rounded-full ">
+                    0
+                  </span>
+                </div>
+              </li>
+              <li>
+                <a
+                  href="/store"
+                  className="flex items-center p-2 text-base font-normal rounded-lg  hover:bg-azure-500"
+                >
+                  <ShoppingBagIcon className="w-5" />
+                  <span className="flex-1 ml-3 whitespace-nowrap">Store</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/banking"
+                  className="flex items-center p-2 text-base font-normal rounded-lg  hover:bg-azure-500"
+                >
+                  <BanknotesIcon className="w-5" />
+                  <span className="flex-1 ml-3 whitespace-nowrap">Banking</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/insurance"
+                  className="flex items-center p-2 text-base font-normal rounded-lg  hover:bg-azure-500"
+                >
+                  <HomeIcon className="w-5" />
+                  <span className="flex-1 ml-3 whitespace-nowrap">
+                    Insurance
+                  </span>
+                </a>
+              </li>
+            </ul>
           </div>
+        </aside>
+        <div className="mx-auto bg-white border  rounded-md m-4 w-full ml-5 p-5">
+          <div className="mb-4 font-bold text-xl">My Orders</div>
+          {/* table */}
+          <div className="overflow-x-auto relative shadow-sm sm:rounded-lg">
+            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-200">
+                <tr>
+                  <th scope="col" className="py-3 px-6">
+                    Order
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                    Status
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                    Total Items
+                  </th>
+                  <th scope="col" className="py-3 px-6">
+                    Total Cost
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {orders?.map((order) => (
+                  <tr key={order.id} className="border-b bg-gray-100 ">
+                    <th
+                      scope="row"
+                      className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap "
+                    >
+                      #{order.id}
+                    </th>
+                    <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap">
+                      {order.status}
+                    </td>
+                    <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap">
+                      {order.totalItems}
+                    </td>
+                    <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap">
+                      ${order.cartTotal}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+
+          <div className="my-4 font-bold text-xl">Bank Accounts</div>
+          <div className="my-4 font-bold text-xl">Insurance Policies</div>
         </div>
       </div>
     </div>

742ebee3e001e6f9e474f4c023417c1e8b33d2b1
Shawn Pearson <1157wcl@gmail.com>
2022-09-15 11:18:54 -0400

    mongo

---
+++ APIquotes/www/html/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>  
+     <head>  
+      <title>Hello World!</title>
+     </head>  
+
+     <body>  
+      <h1>Hello World!</h1>
+      <p><?php echo 'We are running PHP, version: ' . phpversion(); ?></p>
+     </body>

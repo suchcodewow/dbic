a8c4b75ed8dbe83f557ede363db6c2bd63baa626
Shawn Pearson <1157wcl@gmail.com>
2022-11-10 15:01:31 -0500

    working single test

--- loadgen/tests/dbic.spec.js
+++ loadgen/tests/dbic.spec.js
@@ -1,55 +1,42 @@
 import { test, expect } from "@playwright/test";
 
-test.beforeEach(async ({ page }) => {
-  // Runs before each test and signs in each page.
-  await page.goto("https://github.com/login");
-  await page.getByText("Login").click();
-  await page.getByLabel("User name").fill("username");
-  await page.getByLabel("Password").fill("password");
-  await page.getByText("Submit").click();
-});
-
-test("PayBill", async ({ page }) => {
-  await page.goto("http://localhost/");
-
+const frontendURL = process.env.frontendURL ? "http://" + process.env.frontendURL : "http://localhost";
+test("test", async ({ page }) => {
+  await page.goto(frontendURL);
   await page.getByRole("link", { name: "Sign In" }).click();
-  await expect(page).toHaveURL("http://localhost/login");
-
+  await expect(page).toHaveURL(frontendURL + "/login");
   await page.getByRole("button", { name: "Sign In" }).click();
-  await expect(page).toHaveURL("http://localhost/");
-
-  await page.locator('nav:has-text("DynaBankInsuraCart")').getByRole("link", { name: "Banking" }).click();
-  await expect(page).toHaveURL("http://localhost/banking");
-
+  await expect(page).toHaveURL(frontendURL + "/");
+  await page.getByRole("navigation").getByText("Banking").click();
+  await expect(page).toHaveURL(frontendURL + "/banking");
   await page.getByText("Transfer & Pay").click();
-
   await page.getByLabel("Payee").click();
-
-  await page.getByLabel("Payee").fill("Tim's Tulip");
-
-  await page.getByLabel("Payee").press("Tab");
-
-  await page.getByRole("combobox", { name: "From Account" }).press("Tab");
-
+  await page.getByLabel("Payee").fill("Bob's Beards");
+  await page.getByLabel("Payment Amount").click();
   await page.getByLabel("Payment Amount").fill("100");
-
   await page.getByRole("button", { name: "Send Payment" }).click();
-});
-
-test("SubmitQuote", async ({ page }) => {
-  await page.goto("http://localhost/");
-
-  await page.getByRole("link", { name: "Sign In" }).click();
-  await expect(page).toHaveURL("http://localhost/login");
-
-  await page.getByRole("button", { name: "Sign In" }).click();
-  await expect(page).toHaveURL("http://localhost/");
-
-  await page.getByRole("link", { name: "Insurance" }).click();
-  await expect(page).toHaveURL("http://localhost/insurance");
-
+  await page.getByText("Insurance").click();
+  await expect(page).toHaveURL(frontendURL + "/insurance");
   await page.getByRole("link", { name: "Let's go!" }).click();
-  await expect(page).toHaveURL("http://localhost/quote");
-
+  await expect(page).toHaveURL(frontendURL + "/quote");
   await page.getByRole("button", { name: "Next" }).click();
+  await page.getByRole("button", { name: "Next" }).click();
+  await page.getByRole("button", { name: "Submit Quote" }).click();
+  await expect(page).toHaveURL(frontendURL + "/myaccount");
+  await page.getByRole("navigation").getByText("Store").click();
+  await expect(page).toHaveURL(frontendURL + "/store");
+  await page
+    .getByRole("heading", {
+      name: "Adazzo 4 in 1 Rolling Makeup Trolley Train Case Cosmetic Suitcase Nail Tech Box for Makeup Artist, Hairstylists, Nail Tech Students Barber Case with with Keys Swivel Wheels - Shiny Pink",
+    })
+    .locator("span")
+    .click();
+  await page.getByRole("button", { name: "Add to bag" }).click();
+  await page.getByText("View Cart").click();
+  await page.getByText("Checkout").nth(2).click();
+  await page.getByText("Complete Order").click();
+  await expect(page).toHaveURL(new RegExp("^http://localhost/myaccount"));
+  await page.getByRole("button", { name: new RegExp("^Open user menu ") }).click();
+  await page.getByRole("menuitem", { name: "Sign out" }).click();
+  await expect(page).toHaveURL(frontendURL + "/");
 });

cdee33cfbafbe4b217fcea69596683975d54c207
SuchCodeWow <1157wcl@gmail.com>
2023-01-24 08:33:58 -0500

    tweaks

--- loadgen/test.js
+++ loadgen/test.js
@@ -2,6 +2,11 @@ module.exports = { test };
 const { expect } = require("@playwright/test");
 const frontendURL = process.env.frontendURL ? "http://" + process.env.frontendURL : "http://localhost";
 async function test(page) {
-  await page.goto(frontendURL);
-  await new Promise((r) => setTimeout(r, 6000));
+  google(page);
+  await page.goto("http://www.osu.edu");
+  await new Promise((r) => setTimeout(r, 2000));
+}
+async function google(page) {
+  await page.goto("http://www.google.com");
+  await new Promise((r) => setTimeout(r, 2000));
 }

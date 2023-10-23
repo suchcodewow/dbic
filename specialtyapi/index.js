e70a56e26c91cf13956e1796fcb10aaecf5e7080
Shawn Pearson <1157wcl@gmail.com>
2023-10-23 12:36:50 -0400

    +nodemon

--- specialtyapi/index.js
+++ specialtyapi/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
+const dbUrl = process.env.DATABASE_URL;
+mongoose.connect(dbUrl);
+const database = mongoose.connection;
+database.on("error", (error) => {
+  console.log(error);
+});
 const app = express();
+
 app.use(express.json());
 app.listen(3000, () => {
   console.log("The server is active on port 3000");
 });
+console.log(dbUrl);

ef6edc42cad75e0543dc4ecf4739d198f0189d95
Shawn Pearson <1157wcl@gmail.com>
2023-10-24 13:08:53 -0400

    mongo update

--- specialtyapi/index.js
+++ specialtyapi/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const router = express.Router();
 
+// Database connection
 const dbUrl = process.env.DATABASE_URL;
 mongoose.connect(dbUrl);
 const database = mongoose.connection;
@@ -9,6 +11,42 @@ database.on("error", (error) => {
   console.log(error);
 });
 const app = express();
+
+// Define schema
+const Schema = mongoose.Schema;
+
+const CustomQuoteSchema = new Schema({
+  a_string: String,
+  a_date: Date,
+});
+
+// Compile model from schema
+const CustomQuote = mongoose.model("CustomQUote", CustomQuoteSchema);
+
+//Post Method
+router.post("/post", (req, res) => {
+  res.send("Post API");
+});
+
+//Get all Method
+router.get("/getAll", (req, res) => {
+  res.send("Get All API");
+});
+
+//Get by ID Method
+router.get("/getOne/:id", (req, res) => {
+  res.send("Get by ID API");
+});
+
+//Update by ID Method
+router.patch("/update/:id", (req, res) => {
+  res.send("Update by ID API");
+});
+
+//Delete by ID Method
+router.delete("/delete/:id", (req, res) => {
+  res.send("Delete by ID API");
+});
 
 app.use(express.json());
 app.listen(3000, () => {

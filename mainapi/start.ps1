9b1353594fae499704bcaec4638be97de5711151
Shawn Pearson <1157wcl@gmail.com>
2023-12-29 21:36:11 -0500

    add some start.ps1 to the things

---
+++ mainapi/start.ps1
@@ -0,0 +1,6 @@
+if (-not(Test-Path -Path  ".\.venv\Scripts\Activate.ps1")) {
+    Invoke-expression -Command "python -m venv .\.venv"
+}
+Invoke-expression -Command ".\.venv\Scripts\Activate.ps1"
+Invoke-expression -Command "pip install -r requirements.txt"
+Invoke-expression -Command "python -m flask run"

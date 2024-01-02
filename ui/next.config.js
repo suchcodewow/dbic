98a363972e35eb0e8f86fd9b4596d18b1536e6d5
Shawn Pearson <1157wcl@gmail.com>
2024-01-03 04:08:51 +0700

    fix config

--- ui/next.config.js
+++ ui/next.config.js
@@ -35,7 +35,7 @@ const nextConfig = {
         destination: "http://127.0.0.1:6000/quote/:path*",
       },
       {
-        source: "/devapi/api/:path*",
+        source: "/devapi/main/:path*",
         destination: "http://127.0.0.1:5000/api/:path*",
       },
       {
